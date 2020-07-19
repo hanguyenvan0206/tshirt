49bb1879bc4d92a211365d3fbeae32ec6830ed16
hanv <hanv@ttc-solutions.com.vn>
2020-07-19 10:19:03 +0700

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  Api:
+  API:'https://tshirt26.herokuapp.com/',
+  imgAPI: 'https://i.ibb.co/',
   production: false
 };
 
